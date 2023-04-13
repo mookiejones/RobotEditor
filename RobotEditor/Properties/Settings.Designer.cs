ba05e1dd2008bf012062a23ec259c831e669784e
Charles Berman <Charles.Heath.Berman@Gmail.com>
2023-04-12 20:48:54 -0400

    update action

--- RobotEditor/Properties/Settings.Designer.cs
+++ RobotEditor/Properties/Settings.Designer.cs
@@ -8,54 +8,55 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RobotEditor.Properties; 
-
-
-[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
-internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+namespace RobotEditor.Properties {
     
-    private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
     
-    public static Settings Default {
-        get {
-            return defaultInstance;
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
         }
-    }
-    
-    [global::System.Configuration.UserScopedSettingAttribute()]
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Configuration.DefaultSettingValueAttribute("")]
-    public string OpenDocuments {
-        get {
-            return ((string)(this["OpenDocuments"]));
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string OpenDocuments {
+            get {
+                return ((string)(this["OpenDocuments"]));
+            }
+            set {
+                this["OpenDocuments"] = value;
+            }
         }
-        set {
-            this["OpenDocuments"] = value;
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public string Filter {
+            get {
+                return ((string)(this["Filter"]));
+            }
+            set {
+                this["Filter"] = value;
+            }
         }
-    }
-    
-    [global::System.Configuration.UserScopedSettingAttribute()]
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Configuration.DefaultSettingValueAttribute("1")]
-    public string Filter {
-        get {
-            return ((string)(this["Filter"]));
-        }
-        set {
-            this["Filter"] = value;
-        }
-    }
-    
-    [global::System.Configuration.UserScopedSettingAttribute()]
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Configuration.DefaultSettingValueAttribute("")]
-    public string CheckForUpdates {
-        get {
-            return ((string)(this["CheckForUpdates"]));
-        }
-        set {
-            this["CheckForUpdates"] = value;
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string CheckForUpdates {
+            get {
+                return ((string)(this["CheckForUpdates"]));
+            }
+            set {
+                this["CheckForUpdates"] = value;
+            }
         }
     }
 }
